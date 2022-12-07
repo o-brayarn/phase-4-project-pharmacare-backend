86e722152905c27779b426bf49ab6d50103a2dae
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:41:06 +0300

    destroy session via logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
@@ -7,3 +8,9 @@
       render json: { error: "Invalid email or password" }, status: :unauthorized
     end
   end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+end
