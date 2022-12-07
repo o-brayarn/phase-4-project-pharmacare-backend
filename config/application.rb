24e4dcc52613a32f44f0fa7b4634f32a58983241
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:45:39 +0300

    configure cookies

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ Bundler.require(*Rails.groups)
 
 module PharmacareApi
   class Application < Rails::Application
+    # Adding cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
