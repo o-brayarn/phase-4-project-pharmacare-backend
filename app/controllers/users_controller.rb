14609af02e48c297a56cb5d7b27157b3feb3be44
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:41:52 +0300

    enable account registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
   end
 end
