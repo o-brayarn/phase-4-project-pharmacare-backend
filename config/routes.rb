bbe5e916d1b8da643ce7ea00e4a3fee0a9e96ce0
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:46:27 +0300

    add login, logout, & register routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :medicines
   resources :users
   resources :pharmacies
+  post "/register", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
