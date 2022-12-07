f58e0b032ba01f9d848536cfa0494b071c024d78
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:40:09 +0300

    show a single pharmacy with available meds

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,8 +1,25 @@
 class PharmaciesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   before_action :authorize
+  # skip_before_action :authorize, only: [:index, :show]
+
   def index
     @pharmacies = Pharmacy.all
-    render json: @pharmacies
+    render json: @pharmacies, status: :ok
+  end
+
+  def show
+    @pharmacy = Pharmacy.find(params[:id])
+    render json: @pharmacy, status: :ok, serializer: PharmacyMedicinesSerializer
+  end
+
+  private
+
+  def authorize
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_not_found_response(invalid)
+    render json: { errors: invalid }, status: :not_found
   end
 end
