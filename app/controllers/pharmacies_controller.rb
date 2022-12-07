7ebb41623de4a862de8478693ab998bc45900a31
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:39:34 +0300

    rescue not_found response

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,4 +1,6 @@
 class PharmaciesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  before_action :authorize
   def index
     @pharmacies = Pharmacy.all
     render json: @pharmacies
