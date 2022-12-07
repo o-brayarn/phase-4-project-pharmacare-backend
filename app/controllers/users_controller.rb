3e6e26fde0900bbd1dde5b41d1b43391c6ecf6fc
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 08:42:42 +0300

    show individual users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  def create
     user = User.create(user_params)
     if user.valid?
       session[:user_id] = user.id
@@ -7,5 +8,19 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :location, :phone_number, :password, :password_confirmation)
   end
 end
