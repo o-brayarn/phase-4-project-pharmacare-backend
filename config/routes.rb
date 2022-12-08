1d9b244177b8738265b0d56526a4af86f3d4349b
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-08 08:54:03 +0300

    add users route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post "/register", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
